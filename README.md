555bf7b0695fa71b34b0bcdef1c776698e0f1c41
pofider <jan.blaha@hotmail.com>
2020-05-12 20:42:23 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# jsreport .net docker-compose example
+
+Simply open in VS and run docker with F5.   
+
+The example shows how you can run jsreport outside your app what is generally a better practice. It shows how you can use the [jsreport.AspNetCore](https://jsreport.net/learn/dotnet-aspnetcore) and Razor views to render reports through "external" jsreport docker container.    
+
+Additionally, it shows how you can define reports inside jsreport studio. Mount the data volume so your templates stay in the version control. And how to render such stored reports from your app.
+
+
---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using jsreport.AspNetCore;
+using jsreport.Types;
+using WebApp.Model;
+using System.Net.Http;
+using jsreport.Client;
+using Microsoft.Net.Http.Headers;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [MiddlewareFilter(typeof(JsReportPipeline))]
+        public IActionResult Invoice()
+        {
+            HttpContext.JsReportFeature()
+                // the normal jsreport base url injection into the html doesn't work properly with docker and asp.net because of port mapping
+                // the project typically starts with some http://localhost:1234 url but inside docker the url is http://localhost
+                .Configure((req) => req.Options.Base = "http://webapp")
+                .Recipe(Recipe.ChromePdf);         
+
+            return View(InvoiceModel.Example());
+        }
+
+        [MiddlewareFilter(typeof(JsReportPipeline))]
+        public IActionResult Items()
+        {
+            HttpContext.JsReportFeature()
+                .Recipe(Recipe.HtmlToXlsx);               
+
+            return View(InvoiceModel.Example());
+        }
+
+        // no need to use razor, you can also create templates inside jsreport http://localhost:5488 and render them by name
+        // because the jsreport container data folder is maped volume, you still have everything inside version control
+        private IReportingService _reportingService = new ReportingService("http://jsreport:5488");
+        public async Task<IActionResult> Orders()
+        {
+            var result = await _reportingService.RenderByNameAsync("orders-main", null);
+
+            return new FileStreamResult(result.Content, new MediaTypeHeaderValue(result.Meta.ContentType));
+        }
+    }
+}
